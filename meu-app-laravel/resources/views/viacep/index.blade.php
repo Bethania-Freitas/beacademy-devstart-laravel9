0389f3d86b341de2b9db62498c650c6ab152d98b
= <bethaniafreitas@yahoo.com.br>
2022-06-27 15:37:31 -0300

    atualizando as aulas

---
+++ meu-app-laravel/resources/views/viacep/index.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Via cep</title>
+</head>
+<body>
+    <div>
+        <form action="{{ route('viacep.index.post') }}" method="post">
+            @csrf
+            <input type="text" name="cep">
+            <button type="submit" value="Enviar">ENVIAR</button>
+        </form>
+    </div>
+</body>
+</html>
--- meu-app-laravel/routes/web.php
+++ meu-app-laravel/routes/web.php
@@ -1,8 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
+use App\Http\Controllers\{
+    UserController,
+    ViaCepController
+};
 
 
 Route::get('/users', [UserController::class, 'index'])->name('users.index');
 Route::get('/users/{id}', [UserController::class, 'show'])->name('users.show');
+
+
+//VIA CEP WEB SERVICE
+Route::get('/viacep', [ViaCepController::class, 'index'])->name('viacep.index');
+Route::post('/viacep', [ViaCepController::class, 'index'])->name('viacep.index.post');
+Route::get('/viacep/{cep}', [ViaCepController::class, 'show'])->name('viacep.show');
